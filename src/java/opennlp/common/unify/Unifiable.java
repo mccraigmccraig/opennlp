78099815bf13406a154294f4f28416e80410915b
jasonbaldridge <jasonbaldridge>
2001-11-26 18:46:53 +0000

    Added unifyCheck method to interface for a quick way to determine if unification is remotely possible.

--- src/java/opennlp/common/unify/Unifiable.java
+++ src/java/opennlp/common/unify/Unifiable.java
@@ -9,7 +9,7 @@
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Lesser General Public License for more details.
 // 
 // You should have received a copy of the GNU Lesser General Public
 // License along with this program; if not, write to the Free Software
@@ -22,9 +22,8 @@ package opennlp.common.unify;
  * An interface for classes that may be unified.
  *
  * @author      Gann Bierner
- * @version     $Revision: 1.3 $, $Date: 2001/11/22 15:04:50 $
+ * @version     $Revision: 1.4 $, $Date: 2001/11/26 18:46:53 $
  **/
-
 public interface Unifiable {
    
     /**
@@ -47,6 +46,26 @@ public interface Unifiable {
      *         this Unifiable with the Object
      */
     public Object unify (Object o, Substitution s) throws UnifyFailure;
+
+    /**
+     * Check if this Unifiable can unify with another Object.  This
+     * should be implemented as a quick check to allow users of the
+     * Unifiable to scan a group of Unifications to rapidly see if the
+     * entire group is at least possible before descending into each
+     * one with a full unification procedure.  Thus, if a call to this
+     * method does not result in a UnifyFailure exception being
+     * thrown, it doesn't mean that the Object can definitely be
+     * unified with this Unifiable -- what is important is that when a
+     * call to this method throws a UnifyFailure exception, it permits
+     * one to avoid calling the unify() method on other Unifiables in
+     * a group because the quick check failed on this one.
+     *
+     * @param o object to check for unifiability
+     * @exception UnifyFailure if this Unifiable cannot be unified with 
+     *            the Object
+     * @return the Object o, unmodified 
+     **/
+    public Object unifyCheck (Object o) throws UnifyFailure;
 
     /**
      * Replaces any variables in this Unifiable with the values found
