af100d648b2a808919bfe8d3ec69222e307e935b
jasonbaldridge <jasonbaldridge>
2001-11-29 13:12:05 +0000

    Lots of changes.  Added default constructor, static variables to refer to element names, and methods for retrieving particular element types. The toString() method now returns the normalized text of the document, and what it formerly did is now called toXml().

--- src/java/opennlp/common/xml/NLPDocument.java
+++ src/java/opennlp/common/xml/NLPDocument.java
@@ -23,6 +23,7 @@ import org.jdom.*;
 import org.jdom.input.*;
 import org.jdom.output.*;
 import java.io.*;
+import java.util.*;
 
 
 /**
@@ -30,32 +31,51 @@ import java.io.*;
  * specifications.
  *
  * @author      Jason Baldridge
- * @version     $Revision: 1.5 $, $Date: 2001/11/29 11:11:04 $
- */
+ * @version     $Revision: 1.6 $, $Date: 2001/11/29 13:12:05 $
+ **/
 public class NLPDocument extends Document {
+    public static final String WORD_LABEL = "w";
+    public static final String TOKEN_LABEL = "t";
+    public static final String SENTENCE_LABEL = "s";
+    public static final String PARAGRAPH_LABEL = "p";
+    
+    private Element _root;
+    private int _textLength = 0;
+    
+    /**
+     * Empty arg constructor provided for those who do not want the default
+     * behavior of the constructor which takes a string argument.  Creates the
+     * root element "nlpDocument" and nothing else.
+     **/
+    public NLPDocument () {
+	setRootElement(new Element("nlpDocument"));
+    }
 
+    
    /**
-    * Constructor method.
-    * 
-    * Creates a NLPDocument object from a given text.
-    * It breaks the text into paragraphs and "quasiWords" 
-    * (chunks of text separated by white spaces).
-    */
-    public NLPDocument(String text) {
-	super();
-	
-	Element root = new Element("nlpDocument");
+    * Constructor which creates an NLPDocument object from a given text.  It
+    * automatically breaks the text into paragraphs and "quasiWords" (chunks
+    * of text separated by white spaces).  If this behavior is not desired,
+    * use the empty arg constructor and build up the document from the root
+    * element.
+    *
+    * @param text The text which is to be placed into this NLPDocument and
+    * manipulated by tools which process NLPDocuments.
+    **/
+    public NLPDocument (String text) {
+	this();
+	_textLength = text.length();
 	Element textEl = new Element("text");
 	String[] paragraphs, quasiWords;
 	    
 	paragraphs = PerlHelp.getParagraphs(text);
 	for (int i=0; i<paragraphs.length; i++) {
-	    Element paraEl = new Element("p");
-	    Element sentEl = new Element("s");
+	    Element paraEl = new Element(PARAGRAPH_LABEL);
+	    Element sentEl = new Element(SENTENCE_LABEL);
 	    quasiWords = PerlHelp.splitByWhitespace(paragraphs[i]);
 	    for (int j=0; j<quasiWords.length; j++) {
-		Element tokenEl = new Element("t");
-		Element wordEl = new Element("w");
+		Element tokenEl = new Element(TOKEN_LABEL);
+		Element wordEl = new Element(WORD_LABEL);
 		wordEl.setText(quasiWords[j]);
 		tokenEl.addContent(wordEl);
 		sentEl.addContent(tokenEl);
@@ -63,19 +83,164 @@ public class NLPDocument extends Document {
 	    paraEl.addContent(sentEl);
 	    textEl.addContent(paraEl);
 	}
-	root.addContent(textEl);
-	setRootElement(root);
+	_root.addContent(textEl);
+    }
+
+    /**
+     * Sets the root element of this document while maintaining the pointer
+     * which the NLPDocument object keeps to the root Element.
+     **/
+    public Document setRootElement (Element root) {
+	_root = root;
+	return super.setRootElement(root);
+    }
+    
+    
+    /**
+     *  Builds a String[] with each item being a paragraph.
+     **/
+    public String[] getParagraphs () {
+	List parEls = getParagraphElements();
+	String[] pars = new String[parEls.size()];
+	int index = 0;
+	for (Iterator i=parEls.iterator(); i.hasNext();) {
+	    StringBuffer sb = new StringBuffer();
+	    String[] sents = getSentences((Element)i.next());
+	    for (int j=0; j<sents.length; j++)
+		sb.append(sents[j]).append(' ');
+	    pars[index++] = sb.toString().trim();
+	}
+	return pars;
     }
 
     
     /**
+     * Grabs all paragraph elements in this document.
+     **/
+    public List getParagraphElements () {
+	return XmlUtils.getChildrenNested(_root, PARAGRAPH_LABEL);
+    }
+
+    
+    /**
+     * Grabs all sentence elements in this document.
+     **/
+    public String[] getSentences () {
+	return getSentences(_root);
+    }
+
+    
+    /**
+     * Builds a String[] with each item being a sentence.
+     **/
+    public String[] getSentences (Element e) {
+	List sentEls = getSentenceElements(e);
+	String[] sents = new String[sentEls.size()];
+	int index = 0;
+	for (Iterator i=sentEls.iterator(); i.hasNext();) {
+	    List tokEls = getTokenElements((Element)i.next());
+	    StringBuffer sent = new StringBuffer();
+	    for (Iterator j=tokEls.iterator(); j.hasNext();) {
+		List wordEls = getWordElements((Element)j.next());
+		if (wordEls.size() == 1) {
+		    sent.append(((Element)wordEls.get(0)).getText()).append(' ');
+		}
+		else {
+		    for (Iterator k=wordEls.iterator(); k.hasNext();)
+			sent.append(((Element)k.next()).getText()).append(' ');
+		}
+	    }	    
+	    sents[index++] = sent.toString().trim();
+	}
+	return sents;
+    }
+
+    
+    /**
+     * Grabs all sentence elements below the given element.
+     **/
+    public List getSentenceElements (Element e) {
+	return XmlUtils.getChildrenNested(e, SENTENCE_LABEL);
+    }
+
+    /**
+     * Grabs all sentence elements in this document.
+     **/
+    public List getSentenceElements () {
+	return getSentenceElements(_root);
+    }
+    
+    /**
+     * Grabs all token elements below the given element;
+     **/
+    public List getTokenElements (Element e) {
+	return XmlUtils.getChildrenNested(e, TOKEN_LABEL);
+    }
+    
+    /**
+     * Grabs all token elements in this document.
+     **/
+    public List getTokenElements () {
+	return getTokenElements(_root);
+    }
+
+    /**
+     * Grabs all word elements below the given element;
+     **/
+    public List getWordElements (Element e) {
+	return XmlUtils.getChildrenNested(e, WORD_LABEL);
+    }
+    
+    /**
+     * Grabs all word elements in this document.
+     **/
+    public List getWordElements () {
+	return getWordElements(_root);
+    }
+    
+    /**
+     * Creates a nicely indented String showing the XML content of this
+     * NLPDocument.
+     **/
+    public String toXml () {
+        StringWriter sw = new StringWriter();
+	
+          try {
+              new XMLOutputter("  ", true).output(this, sw);
+          }
+          catch (Exception e) {
+              System.out.println("Unable to print document.");
+              System.out.println(e);
+          }
+	  
+	  return sw.toString();
+    }
+
+
+    /**
+     * Outputs the normalized text content of this NLPDocument.
+     **/
+    public String toString () {
+	StringBuffer sb = new StringBuffer(_textLength);
+	List parEls = getParagraphElements();
+	for (Iterator i=parEls.iterator(); i.hasNext();) {
+	    String[] sents = getSentences((Element)i.next());
+	    for (int j=0; j<sents.length; j++)
+		sb.append(sents[j]);
+	    sb.append("\n\n");
+	}
+	return sb.toString().trim();
+    }
+
+
+    /**
      * Create a SENT element for an NLP Document.
      * 
      * @param sent the string of text associated with this sentence.
-     */
+     **/
     public static Element createSENT(String sent) {
         int l = sent.length();
-        Element sentEl = new Element("s");
+        Element sentEl = new Element(SENTENCE_LABEL);
         if (l>0) {
             char delim = sent.charAt(l-1);
             if(delim == '.' || delim == '?' || delim == '!') {
@@ -91,34 +256,20 @@ public class NLPDocument extends Document {
 
 
     /**
-     * Create a TOK element for an NLP Document.
+     * Create a TOK element for an NLPDocument.
      * 
      * @param tok the string of text associated with this token.
-     */
+     **/
     public static Element createTOK(String tok) {
-        Element tokEl = new Element("t");
-        Element lexEl = new Element("w");
-	lexEl.setText(tok);
-        tokEl.addContent(lexEl);
-        return tokEl;
+        return
+	    new Element(TOKEN_LABEL).addContent(
+					new Element(WORD_LABEL).setText(tok));
     }
 
-    public String toString() {
-        StringWriter sw = new StringWriter();
-	
-          try {
-              new XMLOutputter("  ", true).output(this, sw);
-          }
-          catch (Exception e) {
-              System.out.println("Unable to print document.");
-              System.out.println(e);
-          }
-	  
-	  return sw.toString();
-    }
 
     public static void main (String[] args) {
 	NLPDocument doc = new NLPDocument("Here is a sentence. And this is another one.\n\nThis is a sentence in a new paragraph.");
-	System.out.println(doc.toString());
+	//System.out.println(doc.toString());
+	System.out.println(doc.toXml());
     }
 }
