d2eead0d03ddea2dfd74381008c57096a2beed9b
mratkinson <mratkinson>
2002-03-21 23:08:56 +0000

    Added JavaDoc comments Made fields public.

--- Dictionary/src/java/opennlp/dictionary/wordnet/SnsIndex.java
+++ Dictionary/src/java/opennlp/dictionary/wordnet/SnsIndex.java
@@ -1,11 +1,49 @@
 package opennlp.dictionary.wordnet;
 
+/**
+ *  Structure for sense index file entry.
+ *
+ * @author     Mike Atkinson (mratkinson)
+ * @since      0.1.0
+ * @created    20 March 2002
+ * @version    $Id: SnsIndex.java,v 1.2 2002/03/21 23:08:56 mratkinson Exp $
+ */
 public class SnsIndex {
-    String sensekey;		// sense key.
-    String word;	        // word string.
-    int loc;			// synset offset.
-    int wnsense;		// WordNet sense number.
-    int tag_cnt;		// number of semantic tags to sense.
-    SnsIndex nextsi;		// ptr to next sense index entry.
+    /**
+     *  sense key.
+     *
+     * @since    0.1.0
+     */
+    public String sensekey;
+    /**
+     *  word string.
+     *
+     * @since    0.1.0
+     */
+    public String word;
+    /**
+     *  synset offset.
+     *
+     * @since    0.1.0
+     */
+    public int loc;
+    /**
+     *  WordNet sense number.
+     *
+     * @since    0.1.0
+     */
+    public int wnsense;
+    /**
+     *  number of semantic tags to sense.
+     *
+     * @since    0.1.0
+     */
+    public int tag_cnt;
+    /**
+     *  ptr to next sense index entry.
+     *
+     * @since    0.1.0
+     */
+    public SnsIndex nextsi;
 }
 
--- Dictionary/src/java/opennlp/dictionary/wordnet/SynSet.java
+++ Dictionary/src/java/opennlp/dictionary/wordnet/SynSet.java
@@ -1,42 +1,207 @@
 package opennlp.dictionary.wordnet;
 
-/** Structure for data file synset.
+/**
+ *  Structure for data file synset.<p>
+ *
+ *  The following fields are used if a data structure is returned instead of a text
+ *  buffer.
+ *  <ul>
+ *    <li> nextss</li>
+ *    <li> nextform</li>
+ *    <li> searchtype</li>
+ *    <li> ptrlist</li>
+ *    <li> headword</li>
+ *    <li> headsense</li>
+ *  </ul>
+ *  <p>
+ *
+ *  FIXME: This should probably be broken up into two classes to reflex its different
+ *  uses.
+ *
+ * @author     Mike Atkinson (mratkinson)
+ * @since      0.1.0
+ * @created    20 March 2002
+ * @version    $Id: SynSet.java,v 1.2 2002/03/21 23:09:52 mratkinson Exp $
  */
 public class SynSet {
-    int hereiam;		// current file position.
-    int sstype;			// type of ADJ synset.
-    int fnum;			// file number that synset comes from.
-    String pos;			// part of speech.
-    int wcount;			// number of words in synset.
-    String[] words;		// words in synset.
-    int[] lexid;		// unique id in lexicographer file.
-    int[] wnsns;		// sense number in wordnet.
-    int whichword;		// which word in synset we're looking for.
-    Pointer[] pointers;		// pointer types.
-    Frame[] frames;		// frame types.
-    String defn;		// synset gloss (definition).
+    /**
+     *  // current file position.
+     *
+     * @since    0.1.0
+     */
+    public int hereiam;
+    /**
+     *  type of ADJ synset.
+     *
+     * @since    0.1.0
+     */
+    public int sstype;
+    /**
+     *  file number that synset comes from.
+     *
+     * @since    0.1.0
+     */
+    public int fnum;
+    /**
+     *  part of speech.
+     *
+     * @since    0.1.0
+     */
+    public String pos;
+    /**
+     *  number of words in synset.
+     *
+     * @since    0.1.0
+     */
+    public int wcount;
+    /**
+     *  words in synset.
+     *
+     * @since    0.1.0
+     */
+    public String[] words;
+    /**
+     *  unique id in lexicographer file.
+     *
+     * @since    0.1.0
+     */
+    public int[] lexid;
+    /**
+     *  sense number in wordnet.
+     *
+     * @since    0.1.0
+     */
+    public int[] wnsns;
+    /**
+     *  which word in synset we're looking for.
+     *
+     * @since    0.1.0
+     */
+    public int whichword;
+    /**
+     *  pointer types.
+     *
+     * @since    0.1.0
+     */
+    public Pointer[] pointers;
+    /**
+     *  frame types.
+     *
+     * @since    0.1.0
+     */
+    public Frame[] frames;
+    /**
+     *  ynset gloss (definition).
+     *
+     * @since    0.1.0
+     */
+    public String defn;
 
     // these fields are used if a data structure is returned
     // instead of a text buffer.
 
-    SynSet nextss;		// ptr to next synset containing searchword.
-    SynSet nextform;	        // ptr to list of synsets for alternate
-				//   spelling of wordform.
-    int searchtype;		// type of search performed.
-    SynSet ptrlist;		// ptr to synset list result of search.
-    String headword;		// if pos is "s", this is cluster head word.
-    int headsense;		// sense number of headword.
-    
+    /**
+     *  ptr to next synset containing searchword.
+     *
+     * @since    0.1.0
+     */
+    public SynSet nextss;
+    /**
+     *  ptr to list of synsets for alternate spelling of wordform.
+     *
+     * @since    0.1.0
+     */
+    public SynSet nextform;
+
+    /**
+     *  // type of search performed.
+     *
+     * @since    0.1.0
+     */
+    public int searchtype;
+    /**
+     *  ptr to synset list result of search.
+     *
+     * @since    0.1.0
+     */
+    public SynSet ptrlist;
+    /**
+     *  if pos is "s", this is cluster head word.
+     *
+     * @since    0.1.0
+     */
+    public String headword;
+    /**
+     *  sense number of headword.
+     *
+     * @since    0.1.0
+     */
+    public int headsense;
+
+
+    /**
+     *  This contains the pointer information for a SynSet, there might be more
+     *  than one pointer for the SynSet.
+     *
+     * @author     Mike Atkinson (mratkinson)
+     * @since      0.1.0
+     * @created    20 March 2002
+     */
     public static class Pointer {
-        int ptrtyp;		// pointer types.
-        int ptroff;		// pointer offsets.
-        int ppos;		// pointer part of speech.
-        int pto;		// pointer 'to' fields.
-        int pfrm;		// pointer 'from' fields.
+        /**
+         *  pointer types.
+         *
+         * @since    0.1.0
+         */
+        public int ptrtyp;
+        /**
+         *  pointer offset.
+         *
+         * @since    0.1.0
+         */
+        public int ptroff;
+        /**
+         *  pointer part of speech.
+         *
+         * @since    0.1.0
+         */
+        public int ppos;
+        /**
+         *  pointer 'to' field.
+         *
+         * @since    0.1.0
+         */
+        public int pto;
+        /**
+         *  pointer 'from' fields.
+         *
+         * @since    0.1.0
+         */
+        public int pfrm;
     }
+
+
+    /**
+     *  This contains the frame information for a SynSet, there might be more than
+     *  one frame for the SynSet.
+     *
+     * @author     Mike Atkinson (mratkinson)
+     * @since      0.1.0
+     * @created    20 March 2002
+     */
     public static class Frame {
-        int frmid;		// frame numbers.
-        int frmto;		// frame 'to' fields.
+        /**
+         *  frame numbers.
+         *
+         * @since    0.1.0
+         */
+        public int frmid;
+        /**
+         *  frame 'to' fields.
+         *
+         * @since    0.1.0
+         */
+        public int frmto;
     }
 }
 
