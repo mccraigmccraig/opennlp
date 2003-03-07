563b52e05d9045e1ba8da3cd44b14cf40c2ff325
tsmorton <tsmorton>
2003-03-07 03:46:25 +0000

    Tokenizer adapted coresponding from opennlp.grok.preprocessing.tokenize files.

---
+++ src/java/opennlp/tokenize/TokEventCollector.java
@@ -0,0 +1,94 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2000 Jason Baldridge and Gann Bierner
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//////////////////////////////////////////////////////////////////////////////
+
+package opennlp.tokenize;
+
+import opennlp.maxent.Event;
+import opennlp.maxent.EventCollector;
+import opennlp.maxent.ContextGenerator;
+
+import opennlp.common.util.PerlHelp;
+import opennlp.common.util.ObjectIntPair;
+
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.util.ArrayList;
+
+/**
+ * Generate event contexts for maxent decisions for sentence detection.
+ *
+ * @author      Jason Baldridge
+ * @version     $Revision: 1.1 $, $Date: 2003/03/07 03:46:25 $
+ */
+
+public class TokEventCollector implements EventCollector {
+    private static final ContextGenerator cg = new TokContextGenerator();
+    private BufferedReader br;
+    
+    /** 
+     * Class constructor.
+     */
+    public TokEventCollector(Reader data) {
+	br = new BufferedReader(data);
+    }
+
+    
+    /** 
+     * Builds up the list of features using the Reader as input.  For now, this
+     * should only be used to create training data.
+     */
+    public Event[] getEvents() {
+	ArrayList elist = new ArrayList();
+
+	try {
+	    String s = br.readLine();
+	    while (s != null) {
+		String[] spaceToks = PerlHelp.split(s);
+		for (int tok=0; tok<spaceToks.length; tok++) {
+		    StringBuffer sb = new StringBuffer(spaceToks[tok]);
+		    if (!PerlHelp.isAlphanumeric(spaceToks[tok])) {
+			int lastIndex = sb.length()-1;
+			for (int id=0; id<sb.length(); id++) {
+			    String[] context =
+				cg.getContext(new ObjectIntPair(sb, id));
+			    if (id == lastIndex) {
+				elist.add(new Event("T", context));
+			    } else {
+				elist.add(new Event("F", context));
+			    }
+			}
+		    }
+		}
+		s = br. readLine();
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+
+	Event[] events = new Event[elist.size()];
+	elist.toArray(events);
+        return events;
+
+    }
+
+    public Event[] getEvents(boolean evalMode) {
+	return getEvents();
+    }
+    
+}
+
---
+++ src/java/opennlp/tokenize/TokSpanEventStream.java
@@ -0,0 +1,119 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2003 Tom Morton
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//////////////////////////////////////////////////////////////////////////////
+
+package opennlp.tokenize;
+
+import opennlp.maxent.Event;
+import opennlp.maxent.EventStream;
+import opennlp.maxent.ContextGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import opennlp.common.util.Span;
+import opennlp.common.util.PerlHelp;
+import opennlp.common.util.ObjectIntPair;
+
+/** An implementation of EventStream which allows events to be added by 
+ *  offset and returns events for these offset-based tokens.
+ */
+
+public class TokSpanEventStream implements EventStream {
+
+  private static final ContextGenerator cg = new TokContextGenerator();
+  private List events;
+  private int eventIndex;
+  private boolean skipAlphaNumerics;
+  
+  public TokSpanEventStream(boolean skipAlphaNumerics) {
+    this.skipAlphaNumerics = skipAlphaNumerics;
+    events = new ArrayList(50);
+    eventIndex=0;
+  }
+
+  public void addEvents(Span[] tokens,String text) {
+    if (tokens.length > 0) {
+      int start = tokens[0].getStart();
+      int end = tokens[tokens.length-1].getEnd();
+      String sent = text.substring(start,end);
+      List candTokens = TokenizerME.split(sent);
+      int astart=-1;
+      int aend=-1;
+      for (int ci=0;ci<candTokens.size();ci++) {
+	Span cSpan = (Span) candTokens.get(ci);
+	String ctok = sent.substring(cSpan.getStart(),cSpan.getEnd());
+	//adjust cSpan to text offsets
+	cSpan= new Span(cSpan.getStart()+start,cSpan.getEnd()+start);
+	//should we skip this token
+	if (ctok.length() > 1 && (!skipAlphaNumerics || !PerlHelp.isAlphanumeric(ctok))) {
+	  //find offsets of annotated tokens inside candidate tokens
+	  boolean foundTrainingTokens = false;
+	  for (int ti=aend+1;ti<tokens.length;ti++) {
+	    if (cSpan.contains(tokens[ti])) {
+	      if (!foundTrainingTokens) {
+		astart=ti;
+		foundTrainingTokens=true;
+	      }
+	      aend=ti;
+	    }
+	    else if (cSpan.getEnd() < tokens[ti].getEnd()) {
+	      break;
+	    }
+	    else if (tokens[ti].getEnd() < cSpan.getStart()) {
+	      //keep looking
+	    }
+	    else {
+	      System.err.println("Bad training token: "+tokens[ti]+" cand: "+cSpan);
+	    }
+	  }
+	  // create training data
+	  //System.err.println("astart="+astart+" valid="+valid);
+	  if (foundTrainingTokens) {
+	    for (int ti=astart;ti<=aend;ti++) {
+	      Span tSpan = tokens[ti];
+	      //System.err.println("PTBTokenizer.generateEvents: tSpan="+tSpan);
+	      int cStart=cSpan.getStart();
+	      for (int i=tSpan.getStart()+1;i<tSpan.getEnd();i++) {
+		String[] context =cg.getContext(new ObjectIntPair(ctok, i-cStart));
+		events.add(new Event(TokContextGenerator.NO_SPLIT, context));
+	      }
+	      if (tSpan.getEnd() != cSpan.getEnd()) {
+		String[] context =cg.getContext(new ObjectIntPair(ctok, tSpan.getEnd()-cStart));
+		events.add(new Event(TokContextGenerator.SPLIT, context));
+	      }
+	    }
+	  }
+	}
+      }
+    }
+  }
+  
+  public boolean hasNext() {
+    return(eventIndex < events.size());
+  }
+
+  public Event nextEvent() {
+    Event e = (Event) events.get(eventIndex);
+    eventIndex++;
+    if (eventIndex == events.size()) {
+      events.clear();
+      eventIndex=0;
+    }
+    return(e);
+  }
+}
