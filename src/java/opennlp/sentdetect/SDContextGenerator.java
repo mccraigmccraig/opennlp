bd5e5031aca5d3f3f964b77f634a0111b6460c58
tsmorton <tsmorton>
2003-03-07 03:50:09 +0000

    Sentence detection files adapted from opennlp.grok.preprocessing.

---
+++ src/java/opennlp/sentdetect/SDContextGenerator.java
@@ -0,0 +1,223 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2002 Jason Baldridge and Gann Bierner
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//////////////////////////////////////////////////////////////////////////////
+
+package opennlp.sentdetect;
+
+import java.io.*;
+import java.util.*;
+import opennlp.common.util.Pair;
+import opennlp.maxent.ContextGenerator;
+
+/**
+ * Generate event contexts for maxent decisions for sentence detection.
+ *
+ * @author      Jason Baldridge
+ * @author      Eric D. Friedman
+ * @version     $Revision: 1.1 $, $Date: 2003/03/07 03:50:09 $
+ */
+
+public class SDContextGenerator implements ContextGenerator {
+    
+    private StringBuffer buf = new StringBuffer();
+    private List collectFeats = new ArrayList();
+    private Set inducedAbbreviations;
+
+    /**
+     * Creates a new <code>SDContextGenerator</code> instance with
+     * no induced abbreviations.
+     *
+     */
+    public SDContextGenerator() {
+        this(Collections.EMPTY_SET);
+    }
+
+    /**
+     * Creates a new <code>SDContextGenerator</code> instance which uses
+     * the set of induced abbreviations.
+     *
+     * @param inducedAbbreviations a <code>Set</code> of Strings
+     * representing induced abbreviations in the training data.
+     * Example: &quot;Mr.&quot;
+     */
+    public SDContextGenerator(Set inducedAbbreviations) {
+        this.inducedAbbreviations = inducedAbbreviations;
+    }
+        
+    /**
+     * Builds up the list of features, anchored around a position within the
+     * StringBuffer. 
+     */
+    public String[] getContext(Object o) {	
+        String prefix, previous, suffix, next;
+	Object first = ((Pair)o).a;
+	int position = ((Integer)((Pair)o).b).intValue();
+
+	if (first instanceof String[]) {
+	    String[] $_ = (String[])first;
+	    previous = $_[0];
+	    String current = $_[1];
+	    prefix = current.substring(0,position);
+	    suffix = current.substring(position+1);
+	    next = $_[2];
+	}
+	else {
+	    StringBuffer sb = (StringBuffer)((Pair)o).a;
+	    
+	    int lastIndex = sb.length()-1;
+	
+	    int prefixStart = previousSpaceIndex(sb, position);
+	    int c=position;
+	    while(--c > prefixStart)
+		if(sb.charAt(c)=='.') {
+		    prefixStart=c;
+		    break;
+		}
+	    
+	    int prevStart = previousSpaceIndex(sb, prefixStart);
+	    
+	    int suffixEnd = nextSpaceIndex(sb, position, lastIndex);
+	    c=position;
+	    while(++c < suffixEnd) {
+		if(sb.charAt(c)=='.') {
+		    suffixEnd=c;
+		    break;
+		}
+	    }
+	
+	    int nextEnd = nextSpaceIndex(sb, suffixEnd, lastIndex);
+	    
+	    prefix = sb.substring(prefixStart, position).trim();
+	    previous = sb.substring(prevStart, prefixStart).trim();
+	
+	    if (position == lastIndex) {
+		suffix = "";
+		next = "";
+	    } else {
+		suffix = sb.substring(position+1,suffixEnd).trim();
+		next = sb.substring(suffixEnd, nextEnd).trim();
+	    }
+	}
+
+	if (!prefix.equals("")) {
+	    buf.append("x=");
+	    buf.append(prefix);
+	    collectFeats.add(buf.toString());
+	    buf.setLength(0);
+	    collectFeats.add(Integer.toString(prefix.length()));
+            if (isFirstUpper(prefix)) {
+                collectFeats.add("xcap");
+            }
+            if (inducedAbbreviations.contains(prefix)) {
+                collectFeats.add("xabbrev");
+            }
+        }
+
+        if (!previous.equals("")) {
+            buf.append("v=");
+            buf.append(previous);
+            collectFeats.add(buf.toString());
+            buf.setLength(0);
+            if (isFirstUpper(previous)) {
+                collectFeats.add("vcap");
+            }
+            if (inducedAbbreviations.contains(previous)) {
+                collectFeats.add("vabbrev");
+            }
+        }
+
+        if (!suffix.equals("")) {
+            buf.append("s=");
+            buf.append(suffix);
+            collectFeats.add(buf.toString());
+            buf.setLength(0);
+            if (isFirstUpper(suffix)) {
+                collectFeats.add("scap");
+            }
+            if (inducedAbbreviations.contains(suffix)) {
+                collectFeats.add("sabbrev");
+            }
+        }
+
+        if (!next.equals("")) {
+            buf.append("n=");
+            buf.append(next);
+            collectFeats.add(buf.toString());
+            buf.setLength(0);
+            if (isFirstUpper(next)) {
+                collectFeats.add("ncap");
+            }
+            if (inducedAbbreviations.contains(next)) {
+                collectFeats.add("nabbrev");
+            }
+        }
+
+        String[] context = new String[collectFeats.size()];
+        context = (String[])collectFeats.toArray(context);
+        collectFeats.clear();
+        return context;
+    }
+
+    private static final boolean isFirstUpper(String s) {
+        return Character.isUpperCase(s.charAt(0));
+    }
+ 
+    /*
+     * Finds the index of the nearest space before a specified index.
+     *
+     * @param sb   The string buffer which contains the text being examined.
+     * @param seek The index to begin searching from.
+     * @return The index which contains the nearest space.
+     */    
+    private static final int previousSpaceIndex(StringBuffer sb,
+                                                int seek) {
+        seek--;
+        while (seek > 0) {
+            if (sb.charAt(seek) == ' ') {
+                while(seek>0 && sb.charAt(seek-1) == ' ') seek--;
+                return seek;
+            }
+            seek--;
+        }
+        return 0;
+    }
+
+    /*
+     * Finds the index of the nearest space after a specified index.
+     *
+     * @param sb The string buffer which contains the text being examined.
+     * @param seek The index to begin searching from.
+     * @param lastIndex The highest index of the StringBuffer sb.
+     * @return The index which contains the nearest space.
+     */    
+    private static final int nextSpaceIndex(StringBuffer sb,
+                                            int seek,
+                                            int lastIndex) {
+        seek++;
+        char c;
+        while (seek < lastIndex) {
+            c = sb.charAt(seek);
+            if (c == ' ' || c == '\n') {
+                while(sb.length()>seek+1 && sb.charAt(seek+1) == ' ') seek++;
+                return seek;
+            }
+            seek++;
+        }
+        return lastIndex;
+    }
+}
+
---
+++ src/java/opennlp/sentdetect/SDEventStream.java
@@ -0,0 +1,130 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2002 Jason Baldridge and Gann Bierner
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//////////////////////////////////////////////////////////////////////////////   
+package opennlp.sentdetect;
+
+import java.util.Iterator;
+
+import opennlp.common.util.Pair;
+import opennlp.maxent.ContextGenerator;
+import opennlp.maxent.DataStream;
+import opennlp.maxent.Event;
+import opennlp.maxent.EventStream;
+
+/**
+ * An implementation of EventStream which assumes that it is receiving
+ * its data as one (valid) sentence per token.  The default DataStream
+ * to use with this class is PlainTextByLineDataStream, but you can
+ * provide other types of DataStreams if you wish to receive data from
+ * sources other than plain text files; however, be sure that each
+ * token your DataStream returns is a valid sentence.
+ *
+ * @author      Jason Baldridge
+ * @author      Eric D. Friedman
+ * @version     $Revision: 1.1 $, $Date: 2003/03/07 03:50:09 $
+ */
+public class SDEventStream implements EventStream {
+    private DataStream data;
+    private String next;
+    private SDEvent head = null, tail = null;
+    private ContextGenerator cg;
+    private StringBuffer sBuffer = new StringBuffer();
+    private EndOfSentenceScanner scanner;
+
+    /**
+     * Creates a new <code>SDEventStream</code> instance.  A
+     * DefaultEndOfSentenceScanner is used to locate sentence endings.
+     *
+     * @param d a <code>DataStream</code> value
+     */
+    public SDEventStream(DataStream d) {
+        this(d,new DefaultEndOfSentenceScanner(), new SDContextGenerator());
+    }
+    
+    /**
+     * Class constructor which uses the EndOfSentenceScanner to locate
+     * sentence endings.
+     */
+    public SDEventStream (DataStream d, EndOfSentenceScanner s) {
+        this(d,s,new SDContextGenerator());
+    }
+
+    public SDEventStream(DataStream d, EndOfSentenceScanner s, ContextGenerator cg) {
+        data = d;
+        scanner = s;
+        this.cg = cg;
+        if (data.hasNext()) addNewEvents((String)data.nextToken());
+        if (data.hasNext()) next = (String)data.nextToken();
+    }
+
+    public Event nextEvent () {
+        SDEvent top = head;
+        head = head.next;
+        if (null == head) {
+            tail = null;
+        }
+        return top;
+    }
+
+    private void addNewEvents (String s) {
+        StringBuffer sb = sBuffer;
+        sb.append(s.trim());        
+        int sentEndPos = sb.length()-1;
+        if(next !=null && !s.equals("")) {
+            int posAfterFirstWordInNext = next.indexOf(" ");
+            if (posAfterFirstWordInNext != -1) {
+                // should maybe changes this so that it usually adds a space
+                // before the next sentence, but sometimes leaves no space.
+                sb.append(" ");
+                sb.append(next.substring(0, posAfterFirstWordInNext));
+            }
+        }
+
+        for (Iterator i = scanner.getPositions(sb).iterator();
+             i.hasNext();) {
+            Integer candidate = (Integer)i.next();
+            Pair p = new Pair(sb, candidate);
+            String type = (candidate.intValue() == sentEndPos) ? "T" : "F";
+            SDEvent evt = new SDEvent(type,cg.getContext(p));
+
+            if (null != tail) {
+                tail.next = evt;
+                tail = evt;
+            } else if (null == head) {
+                head = evt;
+            } else if (null == head.next) {
+                head.next = tail = evt;
+            }
+        }
+        
+        sb.setLength(0);
+    }
+    
+    public boolean hasNext () {
+        if (null != head) {
+            return true;
+        }
+
+        while (null == head && next != null) {
+            addNewEvents(next);
+            if (data.hasNext())	next = (String)data.nextToken();
+            else next = null;
+        }
+        return (null != head);
+    }
+ 
+}
