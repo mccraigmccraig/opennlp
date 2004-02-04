adc63378c350e3b9818f7dd781b60f4ccf0e11f9
tsmorton <tsmorton>
2004-02-04 15:42:30 +0000

    English Specific Name Finder

---
+++ src/java/opennlp/tools/namefind/EnglishNameFinder.java
@@ -0,0 +1,170 @@
+/*
+ * Created on Jan 29, 2004
+ *
+ */
+package opennlp.tools.namefind;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import opennlp.maxent.MaxentModel;
+import opennlp.maxent.io.SuffixSensitiveGISModelReader;
+import opennlp.tools.util.Span;
+
+/**
+ * @author Tom Morton
+ *
+ */
+public class EnglishNameFinder extends NameFinderME {
+
+  /**
+   * @param mod
+   */
+  public EnglishNameFinder(MaxentModel mod) {
+    super(mod);
+  }
+
+  /**
+   * @param mod
+   * @param cg
+   */
+  public EnglishNameFinder(MaxentModel mod, NameContextGenerator cg) {
+    super(mod, cg);
+  }
+
+  /**
+   * @param mod
+   * @param cg
+   * @param beamSize
+   */
+  public EnglishNameFinder(MaxentModel mod, NameContextGenerator cg, int beamSize) {
+    super(mod, cg, beamSize);
+  }
+
+  public static Span[] tokenizeToSpans(String s) {
+    CharacterEnum charType = CharacterEnum.WHITESPACE;
+    CharacterEnum state = charType;
+
+    List tokens = new ArrayList();
+    int sl = s.length();
+    int start = -1;
+    char pc = 0;
+    for (int ci = 0; ci < sl; ci++) {
+      char c = s.charAt(ci);
+      if (Character.isWhitespace(c)) {
+        charType = CharacterEnum.WHITESPACE;
+      }
+      else if (Character.isLetter(c)) {
+        charType = CharacterEnum.ALPHABETIC;
+      }
+      else if (Character.isDigit(c)) {
+        charType = CharacterEnum.NUMERIC;
+      }
+      else {
+        charType = CharacterEnum.OTHER;
+      }
+      if (state == CharacterEnum.WHITESPACE) {
+        if (charType != CharacterEnum.WHITESPACE) {
+          start = ci;
+        }
+      }
+      else {
+        if (charType != state || (charType == CharacterEnum.OTHER && c != pc)) {
+          tokens.add(new Span(start, ci));
+          start = ci;
+        }
+      }
+      state = charType;
+      pc = c;
+    }
+    if (charType != CharacterEnum.WHITESPACE) {
+      tokens.add(new Span(start, sl));
+    }
+    return ((Span[]) tokens.toArray(new Span[tokens.size()]));
+  }
+
+  public static String[] tokenize(String s) {
+    Span[] spans = tokenizeToSpans(s);
+    String[] tokens = new String[spans.length];
+    for (int si = 0, sl = spans.length; si < sl; si++) {
+      tokens[si] = s.substring(spans[si].getStart(), spans[si].getEnd());
+    }
+    return (tokens);
+  }
+
+  public static void main(String[] args) throws IOException {
+    if (args.length == 0) {
+      System.err.println("Usage EnglishNameFinder model1 model2 < sentnces");
+      System.exit(1);
+    }
+
+    EnglishNameFinder[] finders = new EnglishNameFinder[args.length];
+    String[] names = new String[args.length];
+    for (int ai = 0; ai < args.length; ai++) {
+      String modelName = args[ai];
+      finders[ai] = new EnglishNameFinder(new SuffixSensitiveGISModelReader(new File(modelName)).getModel());
+      int nameStart = modelName.lastIndexOf(System.getProperty("file.separator")) + 1;
+      int nameEnd = modelName.indexOf('.', nameStart);
+      if (nameEnd == -1) {
+        nameEnd = modelName.length();
+      }
+      names[ai] = modelName.substring(nameStart, nameEnd);
+    }
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    String[][] finderTags = new String[finders.length][];
+    for (String line = in.readLine(); null != line; line = in.readLine()) {
+      String[] tokens = tokenize(line);
+      for (int fi = 0, fl = finders.length; fi < fl; fi++) {
+        finderTags[fi] = finders[fi].find(tokens, Collections.EMPTY_MAP);
+        System.err.println(names[fi] + " " + java.util.Arrays.asList(finderTags[fi]));
+      }
+      for (int ti = 0, tl = tokens.length; ti < tl; ti++) {
+        for (int fi = 0, fl = finders.length; fi < fl; fi++) {
+          //check for end tags
+          if (ti != 0) {
+            if ((finderTags[fi][ti].equals(NameFinderME.START) || finderTags[fi][ti].equals(NameFinderME.OTHER)) && (finderTags[fi][ti - 1].equals(NameFinderME.START) || finderTags[fi][ti - 1].equals(NameFinderME.CONTINUE))) {
+              System.out.print("</" + names[fi] + ">");
+            }
+            System.out.print(" ");
+          }
+          //check for start tags
+          if (finderTags[fi][ti].equals(NameFinderME.START)) {
+            System.out.print("<" + names[fi] + ">");
+          }
+        }
+        System.out.print(tokens[ti]);
+      }
+      //final end tags
+      if (tokens.length != 0) {
+        for (int fi = 0, fl = finders.length; fi < fl; fi++) {
+          if (finderTags[fi][tokens.length - 1].equals(NameFinderME.START) || finderTags[fi][tokens.length - 1].equals(NameFinderME.CONTINUE)) {
+            System.out.print("</" + names[fi] + ">");
+          }
+        }
+      }
+      System.out.println();
+    }
+  }
+}
+
+class CharacterEnum {
+  public static final CharacterEnum WHITESPACE = new CharacterEnum("whitespace");
+  public static final CharacterEnum ALPHABETIC = new CharacterEnum("alphabetic");
+  public static final CharacterEnum NUMERIC = new CharacterEnum("numeric");
+  public static final CharacterEnum OTHER = new CharacterEnum("other");
+
+  private String name;
+
+  private CharacterEnum(String name) {
+    this.name = name;
+  }
+
+  public String toString() {
+    return name;
+  }
+}
