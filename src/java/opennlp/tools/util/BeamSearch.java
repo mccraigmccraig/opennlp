935ef1b080d6cf67f63eb8ee7778e5d21ee677b4
tsmorton <tsmorton>
2004-04-30 18:18:06 +0000

    many changes aimed at optimization.

--- src/java/opennlp/tools/util/BeamSearch.java
+++ src/java/opennlp/tools/util/BeamSearch.java
@@ -29,6 +29,9 @@ public class BeamSearch {
   protected MaxentModel model;
   protected BeamSearchContextGenerator cg;
   protected int size;
+  private static final Object[] EMPTY_ADDITIONAL_CONTEXT = new Object[0];
+  private double[] probs;
+  private String out;
 
   /** Creates new search object. 
    * @param size The size of the beam (k).
@@ -39,44 +42,48 @@ public class BeamSearch {
     this.size = size;
     this.cg = cg;
     this.model = model;
+    this.probs = new double[model.getNumOutcomes()];
   }
-  
+
   /** Returns the best sequence of outcomes based on model for this object.
-     * @param numSequences The maximum number of sequences to be returned.
-     * @param sequence The input sequence.
-     * @param additionalContext An Object[] of additional context.  This is passed to the context generator blindly with the assumption that the context are appropiate.
-     * @return An array of the top ranked sequences of outcomes.
-     */
-  public Sequence[] bestSequences(int numSequences,List sequence, Object[] additionalContext) {
-    int n = sequence.size();
-    SortedSet prev = new TreeSet();
-    SortedSet next = new TreeSet();
-    SortedSet tmp;
+   * @param numSequences The maximum number of sequences to be returned.
+   * @param sequence The input sequence.
+   * @param additionalContext An Object[] of additional context.  This is passed to the context generator blindly with the assumption that the context are appropiate.
+   * @return An array of the top ranked sequences of outcomes.
+   */
+  public Sequence[] bestSequences(int numSequences, Object[] sequence, Object[] additionalContext) {
+    int n = sequence.length;
+    Heap prev = new TreeHeap(size);
+    Heap next = new  TreeHeap(size);
+    Heap tmp;
+
     prev.add(new Sequence());
     if (additionalContext == null) {
-      additionalContext = new Object[0];
+      additionalContext = EMPTY_ADDITIONAL_CONTEXT;
     }
     for (int i = 0; i < n; i++) {
       int sz = Math.min(size, prev.size());
-      int sc =0;
-      for (Iterator si = prev.iterator();si.hasNext() && sc<sz;sc++) {
-        Sequence top = (Sequence) si.next();
-        double[] scores = model.eval(cg.getContext(i,sequence,top,additionalContext));
+      int sc = 0;
+      for (; prev.size() > 0 && sc < sz; sc++) {
+        Sequence top = (Sequence) prev.extract();
+        List tmpOutcomes = top.getOutcomes();
+        String[] outcomes = (String[]) tmpOutcomes.toArray(new String[tmpOutcomes.size()]);
+        double[] scores = model.eval(cg.getContext(i, sequence, outcomes, additionalContext), probs);
         double[] temp_scores = new double[scores.length];
 
         for (int c = 0; c < scores.length; c++) {
-          if (!validSequence(i, sequence, top, model.getOutcome(c))) {
-            scores[c] = 0;
-          }
           temp_scores[c] = scores[c];
         }
         Arrays.sort(temp_scores);
-        double min = temp_scores[Math.min(scores.length-1,size)];
+        double min = temp_scores[Math.min(scores.length - 1, size)];
 
         for (int p = 0; p < scores.length; p++) {
           if (scores[p] < min)
             continue; //only advance first "size" outcomes
-          next.add(new Sequence(top,model.getOutcome(p),scores[p]));
+          out = model.getOutcome(p);
+          if (validSequence(i, sequence, outcomes, out)) {
+            next.add(new Sequence(top, out, scores[p]));
+          }
         }
       }
       //    make prev = next; and re-init next (we reuse existing prev set once we clear it)
@@ -85,12 +92,66 @@ public class BeamSearch {
       prev = next;
       next = tmp;
     }
-    int numSeq = Math.min(numSequences,prev.size());
-    List topSequences = new ArrayList(numSeq);
-    for (Iterator si=prev.iterator();numSeq> 0;numSeq--) {
-      topSequences.add(si.next());
+    int numSeq = Math.min(numSequences, prev.size());
+    Sequence[] topSequences = new Sequence[numSeq];
+    int seqIndex = 0;
+    for (; seqIndex < numSeq; seqIndex++) {
+      topSequences[seqIndex] = (Sequence) prev.extract();
     }
-    return (Sequence[]) topSequences.toArray(new Sequence[topSequences.size()]);
+    return topSequences;
+  }
+
+  /** Returns the best sequence of outcomes based on model for this object.
+     * @param numSequences The maximum number of sequences to be returned.
+     * @param sequence The input sequence.
+     * @param additionalContext An Object[] of additional context.  This is passed to the context generator blindly with the assumption that the context are appropiate.
+     * @return An array of the top ranked sequences of outcomes.
+     */
+  public Sequence[] bestSequences(int numSequences, List sequence, Object[] additionalContext) {
+    int n = sequence.size();
+    Heap prev = new TreeHeap(size);
+    Heap next = new TreeHeap(size);
+    Heap tmp;
+    prev.add(new Sequence());
+    if (additionalContext == null) {
+      additionalContext = EMPTY_ADDITIONAL_CONTEXT;
+    }
+    for (int i = 0; i < n; i++) {
+      int sz = Math.min(size, prev.size());
+      int sc = 0;
+      for (; prev.size() > 0 && sc < sz; sc++) {
+        Sequence top = (Sequence) prev.extract();
+        double[] scores = model.eval(cg.getContext(i, sequence, top, additionalContext),probs);
+        double[] temp_scores = new double[scores.length];
+
+        for (int c = 0; c < scores.length; c++) {
+          temp_scores[c] = scores[c];
+        }
+        Arrays.sort(temp_scores);
+        double min = temp_scores[Math.min(scores.length - 1, size)];
+
+        for (int p = 0; p < scores.length; p++) {
+          if (scores[p] < min)
+            continue; //only advance first "size" outcomes
+          out = model.getOutcome(p);
+          if (validSequence(i, sequence, top, out)) {
+            next.add(new Sequence(top, out, scores[p]));
+          }
+        }
+      }
+      //    make prev = next; and re-init next (we reuse existing prev set once we clear it)
+      prev.clear();
+      tmp = prev;
+      prev = next;
+      next = tmp;
+    }
+    int numSeq = Math.min(numSequences, prev.size());
+    Sequence[] topSequences = new Sequence[numSeq];
+    int seqIndex = 0;
+    for (; seqIndex < numSeq; seqIndex++) {
+      topSequences[seqIndex] = (Sequence) prev.extract();
+    }
+    return topSequences;
   }
 
   /** Returns the best sequence of outcomes based on model for this object.
@@ -99,7 +160,16 @@ public class BeamSearch {
    * @return The top ranked sequence of outcomes.
    */
   public Sequence bestSequence(List sequence, Object[] additionalContext) {
-    return bestSequences(1,sequence,additionalContext)[0];
+    return bestSequences(1, sequence, additionalContext)[0];
+  }
+  
+  /** Returns the best sequence of outcomes based on model for this object.
+    * @param sequence The input sequence.
+    * @param additionalContext An Object[] of additional context.  This is passed to the context generator blindly with the assumption that the context are appropiate.
+    * @return The top ranked sequence of outcomes.
+    */
+  public Sequence bestSequence(Object[] sequence, Object[] additionalContext) {
+    return bestSequences(1, sequence, additionalContext)[0];
   }
 
   /** Determines wheter a particular continuation of a sequence is valid.  
@@ -112,6 +182,19 @@ public class BeamSearch {
    * @return true is the sequence would still be valid with the new outcome, false otherwise.
    */
   protected boolean validSequence(int i, List inputSequence, Sequence outcomesSequence, String outcome) {
+    return true;
+  }
+
+  /** Determines wheter a particular continuation of a sequence is valid.  
+     * This is used to restrict invalid sequences such as thoses used in start/continure tag-based chunking 
+     * or could be used to implement tag dictionary restrictions.
+     * @param i The index in the input sequence for which the new outcome is being proposed.
+     * @param inputSequence The input sequnce.
+     * @param outcomesSequence The outcomes so far in this sequence.
+     * @param outcome The next proposed outcome for the outcomes sequence.
+     * @return true is the sequence would still be valid with the new outcome, false otherwise.
+     */
+  protected boolean validSequence(int i, Object[] inputSequence, String[] outcomesSequence, String outcome) {
     return true;
   }
 
