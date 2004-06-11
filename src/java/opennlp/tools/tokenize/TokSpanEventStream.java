fdcb0766d553a356f3bbdfdb9a9b8fc2e8c5024a
tsmorton <tsmorton>
2004-06-11 20:59:18 +0000

    fixed bug in training event generation and re-named some variables.

--- src/java/opennlp/tools/tokenize/TokSpanEventStream.java
+++ src/java/opennlp/tools/tokenize/TokSpanEventStream.java
@@ -31,7 +31,6 @@ import java.util.List;
 /** An implementation of EventStream which allows events to be added by 
  *  offset and returns events for these offset-based tokens.
  */
-
 public class TokSpanEventStream implements EventStream {
 
   private static final ContextGenerator cg = new TokContextGenerator();
@@ -51,8 +50,8 @@ public class TokSpanEventStream implements EventStream {
       int end = tokens[tokens.length - 1].getEnd();
       String sent = text.substring(start, end);
       List candTokens = TokenizerME.split(sent);
-      int astart = -1;
-      int aend = -1;
+      int firstTrainingToken = -1;
+      int lastTrainingToken = -1;
       for (int ci = 0; ci < candTokens.size(); ci++) {
         Span cSpan = (Span) candTokens.get(ci);
         String ctok = sent.substring(cSpan.getStart(), cSpan.getEnd());
@@ -60,16 +59,17 @@ public class TokSpanEventStream implements EventStream {
         cSpan = new Span(cSpan.getStart() + start, cSpan.getEnd() + start);
         //should we skip this token
         if (ctok.length() > 1
-          && (!skipAlphaNumerics || TokenizerME.alphaNumeric.matcher(ctok).matches())) {
-          //find offsets of annotated tokens inside candidate tokens
+          && (!skipAlphaNumerics || !TokenizerME.alphaNumeric.matcher(ctok).matches())) {
+
+          //find offsets of annotated tokens inside of candidate tokens
           boolean foundTrainingTokens = false;
-          for (int ti = aend + 1; ti < tokens.length; ti++) {
+          for (int ti = lastTrainingToken + 1; ti < tokens.length; ti++) {
             if (cSpan.contains(tokens[ti])) {
               if (!foundTrainingTokens) {
-                astart = ti;
+                firstTrainingToken = ti;
                 foundTrainingTokens = true;
               }
-              aend = ti;
+              lastTrainingToken = ti;
             }
             else if (cSpan.getEnd() < tokens[ti].getEnd()) {
               break;
@@ -85,19 +85,15 @@ public class TokSpanEventStream implements EventStream {
           // create training data
           //System.err.println("astart="+astart+" valid="+valid);
           if (foundTrainingTokens) {
-            for (int ti = astart; ti <= aend; ti++) {
+            for (int ti = firstTrainingToken; ti <= lastTrainingToken; ti++) {
               Span tSpan = tokens[ti];
-              //System.err.println("PTBTokenizer.generateEvents: tSpan="+tSpan);
               int cStart = cSpan.getStart();
               for (int i = tSpan.getStart() + 1; i < tSpan.getEnd(); i++) {
-                String[] context =
-                  cg.getContext(new ObjectIntPair(ctok, i - cStart));
+                String[] context = cg.getContext(new ObjectIntPair(ctok, i - cStart));
                 events.add(new Event(TokContextGenerator.NO_SPLIT, context));
               }
               if (tSpan.getEnd() != cSpan.getEnd()) {
-                String[] context =
-                  cg.getContext(
-                    new ObjectIntPair(ctok, tSpan.getEnd() - cStart));
+                String[] context = cg.getContext(new ObjectIntPair(ctok, tSpan.getEnd() - cStart));
                 events.add(new Event(TokContextGenerator.SPLIT, context));
               }
             }
