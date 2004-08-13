9408c74a8f0273f1fc7e90c46bdaea395fc54242
tsmorton <tsmorton>
2004-08-13 16:43:46 +0000

    changed interface to better support caching.

--- src/java/opennlp/tools/chunker/ChunkerContextGenerator.java
+++ src/java/opennlp/tools/chunker/ChunkerContextGenerator.java
@@ -1,18 +1,16 @@
 package opennlp.tools.chunker;
 
-import java.util.List;
-
 import opennlp.tools.util.BeamSearchContextGenerator;
 
 public interface ChunkerContextGenerator extends BeamSearchContextGenerator {
-
+  
   /**
    * Returns the contexts for chunking of the specified index.
    * @param i The index of the token in the specified toks array for which the context should be constructed. 
    * @param toks The tokens of the sentence.  The <code>toString</code> methods of these objects should return the token text.
-   * @param preds The previous decisions made in the taging of this sequence.  Only indices less than i will be examined.
    * @param tags The POS tags for the the specified tokens.
+   * @param preds The previous decisions made in the taging of this sequence.  Only indices less than i will be examined.
    * @return An array of predictive contexts on which a model basis its decisions.
    */
-  public abstract String[] getContext(int i, List toks, List preds, List tags);
+  public String[] getContext(int i, Object[] toks, String[] tags, String[] preds);
 }
