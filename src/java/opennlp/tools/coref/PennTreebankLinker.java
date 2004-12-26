ee210b1cf44d833a976456d10be57f609c726302
tsmorton <tsmorton>
2004-12-26 05:28:52 +0000

    new linker for penn treebank np coref annotation.

---
+++ src/java/opennlp/tools/coref/PennTreebankLinker.java
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////
+//Copyright (C) 2003 Thomas Morton
+//
+//This library is free software; you can redistribute it and/or
+//modify it under the terms of the GNU Lesser General Public
+//License as published by the Free Software Foundation; either
+//version 2.1 of the License, or (at your option) any later version.
+//
+//This library is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Lesser General Public License for more details.
+//
+//You should have received a copy of the GNU Lesser General Public
+//License along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//////////////////////////////////////////////////////////////////////////////
+package opennlp.tools.coref;
+
+import java.io.IOException;
+
+import opennlp.tools.coref.mention.PTBMentionFinder;
+
+public class PennTreebankLinker extends DefaultLinker {
+    
+  
+  
+  public PennTreebankLinker(String project, LinkerMode mode) throws IOException {
+    super(project,mode);
+  }
+  
+  public PennTreebankLinker(String project, LinkerMode mode, boolean useDiscourseModel) throws IOException {
+    super(project,mode,useDiscourseModel);
+  } 
+  
+  public PennTreebankLinker(String project, LinkerMode mode, boolean useDiscourseModel, double fixedNonReferentialProbability) throws IOException {
+    super(project,mode,useDiscourseModel,fixedNonReferentialProbability);
+  }
+  
+  protected void initMentionFinder() {
+    mentionFinder = PTBMentionFinder.getInstance(headFinder);
+  }
+}
