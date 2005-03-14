1cf80990d35f473c6f8de8f665e1c1e187980463
tsmorton <tsmorton>
2005-03-14 06:04:26 +0000

    trimmed functionality and added javadoc.

--- src/java/opennlp/tools/coref/mention/Parse.java
+++ src/java/opennlp/tools/coref/mention/Parse.java
@@ -21,92 +21,122 @@ import java.util.List;
 
 import opennlp.tools.util.Span;
 
+/** Interface for syntactic and named-entity information to be used in coreference annotation. */
 public interface Parse extends Comparable {
+  
+  /**
+   * Returns the index of the sentence which contains this parse.
+   * @return The index of the sentence which contains this parse.
+   */
+  public int getSentenceNumber();
 
-  /** Returns a <code>List</code> of the top-most noun phrases
-   * contained by this object.  The noun phrases in this list should
-   * also implement the <code>Parse</code> interface.  */
+  /** Returns a list of the all noun phrases
+   * contained by this parse.  The noun phrases in this list should
+   * also implement the {@link Parse} interface.  
+   * @return a list of all the noun phrases contained by this parse.
+   */
   public List getNounPhrases();
   
-  /** Returns a <code>List</code> of the top-most named entities
-   * contained by this object.  The named entities in this list should
-   * also implement the <code>Parse</code> interface.  */
+  /** Returns a list of all the named entities
+   * contained by this parse.  The named entities in this list should
+   * also implement the {@link Parse} interface.  
+   * @return a list of all the named entities contained by this parse. */
   public List getNamedEntities();
 
-  /** Returns a <code>List</code> of the children to this object.  The
-   * children should also implement the <code>Parse</code> interface.  */
+  /** Returns a list of the children to this object.  The
+   * children should also implement the {@link Parse} interface.
+   * @return a list of the children to this object.
+   * */
   public List getChildren();
   
+  /**
+   * Returns a list of the children to this object which are constituents or tokens.  The
+   * children should also implement the {@link Parse} interface.  This allows
+   * implementations which contain addition nodes for things such as semantic categories to
+   * hide those nodes from the componets which only care about syntactic nodes. 
+   * @return a list of the children to this object which are constituents or tokens.
+   */
   public List getSyntacticChildren();
 
-  /** Returns a <code>List</code> of the top-most tokens
-   * contained by this object The tokens in this list should also
-   * implement the <code>Parse</code> inteface.  
+  /** Returns a list of the tokens contained by this object.  The tokens in this list should also
+   * implement the {@link Parse} inteface.  
    */
   public List getTokens();
 
-  /** Returns the syntactic type of this node.
-   * @return the syntactic type.  Typically part-of-sppech or 
-   * constituent labeling. */
+  /** Returns the syntactic type of this node. Typically this is the part-of-speech or 
+   * constituent labeling.
+   * @return the syntactic type. 
+   */
   public String getSyntacticType();
-
-  /** Sets the syntactic type of this node. */
-  public void setSyntacticType(String t);
 
   /** Returns the named-entity type of this node.
    * @return the named-entity type. */
   public String getEntityType();
-
-  /** Sets the entity type of this node. */
-  public void setEntityType(String t);
-
-  /** Returns a <code>List</code> of the top-most sub-noun phrases
-     * contained by this object.  The sub-noun phrases in this list should
-     * also implement the <code>Parse</code> interface.  */
-  public List getSubNounPhrases();
   
-  /** Determines whether this has a parent of type NAC.
-   * @return true is this has a parent of type NAC, false otherwise. 
+  /** Determines whether this has an ancestor of type NAC.
+   * @return true is this has an ancestor of type NAC, false otherwise. 
    * */
   public boolean isParentNAC();
   
+  /** Returns the parent parse of this parse node.
+   * @return the parent parse of this parse node.
+   */
   public Parse getParent();
 
-  public void setValidated(boolean v);
-
-  public boolean isValidated();
-
+  /**
+   * Specifies whether this parse is a named-entity.
+   * @return True if this parse is a named-entity; false otherwise.
+   */
   public boolean isNamedEntity();
   
+  /**
+   * Specifies whether this parse is a noun phrase.
+   * @return True if this parse is a noun phrase; false otherwise.
+   */
   public boolean isNounPhrase();
   
+  /**
+   * Specifies whether this parse is a sentence.
+   * @return True if this parse is a sentence; false otherwise.
+   */
   public boolean isSentence();
 
+  /**
+   * Specifies whether this parse is a coordinated noun phrase.
+   * @return True if this parse is a coordinated noun phrase; false otherwise.
+   */
   public boolean isCoordinatedNounPhrase();
 
+  /** Specifies whether this parse is a token.
+   * @return True if this parse is a token; false otherwise.
+   */
   public boolean isToken();
-
-  public List getSentences();
-
-  public List getEntities();
-
-  public String getProperty(String p);
-
-  public void setProperty(String k, String v);
 
   public String toString();
 
+  /** Returns an entity id associated with this parse and coreferent parses.  This is only used for training on
+   * already annotated coreference annotation.
+   * @return an entity id associated with this parse and coreferent parses.
+   */
   public int getEntityId();
-
-  public void setEntityId(int id);
-
-  public void removeEntityId();
 
   /**
    * Returns the character offsets of this parse node.
    * @return The span representing the character offsets of this parse node.
    */
   public Span getSpan();
-
-  public void show();
+  
+  /**
+   * Returns the first token which is not a child of this parse.  If the first token of a sentence is
+   * a child of this parse then null is returned.
+   * @return the first token which is not a child of this parse or null if no such token exists.
+   */
+  public Parse getPreviousToken();
+  
+  /**
+   * Returns the next token which is not a child of this parse.  If the last token of a sentence is
+   * a child of this parse then null is returned.
+   * @return the next token which is not a child of this parse or null if no such token exists.
+   */
+  public Parse getNextToken();
 }
