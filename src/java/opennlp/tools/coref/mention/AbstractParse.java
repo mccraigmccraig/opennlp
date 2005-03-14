ce2ebe1d85b6c3179c4205c022f55994dd05cce5
tsmorton <tsmorton>
2005-03-14 06:05:02 +0000

    Holds common code for most implementations of the Parse interface.

---
+++ src/java/opennlp/tools/coref/mention/AbstractParse.java
@@ -0,0 +1,48 @@
+package opennlp.tools.coref.mention;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sun.rsasign.c;
+
+import opennlp.tools.util.Span;
+
+public abstract class AbstractParse implements Parse {
+
+  public boolean isCoordinatedNounPhrase() {
+    List parts = getSyntacticChildren();
+    if (parts.size() >= 2) {
+      for (int pi = 1; pi < parts.size(); pi++) {
+        Parse child = (Parse) parts.get(pi);
+        String ctype = child.getSyntacticType();
+        if (ctype != null && ctype.equals("CC") && !child.toString().equals("&")) {
+          return (true);
+        }
+      }
+    }
+    return (false);
+  }
+
+  public List getNounPhrases() {
+    List parts = getSyntacticChildren();
+    List nps = new ArrayList();
+    while (parts.size() > 0) {
+      List newParts = new ArrayList();
+      for (int pi=0,pn=parts.size();pi<pn;pi++) {
+        //System.err.println("AbstractParse.getNounPhrases "+parts.get(pi).getClass());
+        Parse cp = (Parse) parts.get(pi);
+        if (cp.isNounPhrase()) {
+          nps.add(cp);
+        }
+        if (!cp.isToken()) {
+          newParts.addAll(cp.getSyntacticChildren());
+        }
+      }
+      parts = newParts;
+    }
+    return nps;
+  }
+  
+  
+
+}
