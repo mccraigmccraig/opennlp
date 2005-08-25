0b60459f5a3cca5b366e5057fa600155c8139265
tsmorton <tsmorton>
2005-08-25 03:20:11 +0000

    cleaned up indentation.

--- src/java/opennlp/tools/postag/POSEventCollector.java
+++ src/java/opennlp/tools/postag/POSEventCollector.java
@@ -39,131 +39,131 @@ import opennlp.tools.util.Pair;
  * An event generator for the maxent POS Tagger.
  *
  * @author      Gann Bierner
- * @version     $Revision: 1.4 $, $Date: 2004/08/13 17:21:30 $
+ * @version     $Revision: 1.5 $, $Date: 2005/08/25 03:20:11 $
  */
 
 public class POSEventCollector implements EventCollector {
-
-    private BufferedReader br;
-    private POSContextGenerator cg;
+  
+  private BufferedReader br;
+  private POSContextGenerator cg;
+  
+  public POSEventCollector(Reader data, POSContextGenerator gen) {
+    br = new BufferedReader(data);
+    cg = gen;
+  }
+  
+  private static Pair split(String s) {
+    int split = s.lastIndexOf("_");
+    if (split == -1) {
+      System.out.println("There is a problem in your training data: "
+          + s
+          + " does not conform to the format WORD_TAG.");
+      return new Pair(s, "UNKNOWN");
+    }
+    return new Pair(s.substring(0, split), s.substring(split+1));
+  }
+  
+  public static Pair convertAnnotatedString(String s) {
+    ArrayList tokens = new ArrayList();
+    ArrayList outcomes = new ArrayList();
+    StringTokenizer st = new StringTokenizer(s);
+    while(st.hasMoreTokens()) {
+      Pair p = split(st.nextToken());
+      tokens.add(p.a);
+      outcomes.add(p.b);
+    }
+    return new Pair(tokens, outcomes);
+  }
+  
+  public Event[] getEvents() {
+    return getEvents(false);
+  }
+  
+  private Set getFrequent(BufferedReader br) {
+    HashMap map = new HashMap();
     
-    public POSEventCollector(Reader data, POSContextGenerator gen) {
-	br = new BufferedReader(data);
-	cg = gen;
-    }
-
-    private static Pair split(String s) {
-	int split = s.lastIndexOf("_");
-	if (split == -1) {
-	    System.out.println("There is a problem in your training data: "
-			       + s
-			       + " does not conform to the format WORD_TAG.");
-	    return new Pair(s, "UNKNOWN");
-	}
-	return new Pair(s.substring(0, split), s.substring(split+1));
-    }
+    try {
+      for(String s = br.readLine(); s!=null; s=br.readLine()) {
+        StringTokenizer st = new StringTokenizer(s);
+        while(st.hasMoreTokens()) {
+          String tok = (String)split(st.nextToken()).a;
+          Counter c = (Counter)map.get(tok);
+          if(c!=null)
+            c.increment();
+          else
+            map.put(tok, new Counter());
+        }
+      }
+    } catch (IOException e) { e.printStackTrace(); }
     
-    public static Pair convertAnnotatedString(String s) {
-	ArrayList tokens = new ArrayList();
-	ArrayList outcomes = new ArrayList();
-	StringTokenizer st = new StringTokenizer(s);
-	while(st.hasMoreTokens()) {
-	    Pair p = split(st.nextToken());
-	    tokens.add(p.a);
-	    outcomes.add(p.b);
-	}
-	return new Pair(tokens, outcomes);
-    }
-
-    public Event[] getEvents() {
-	return getEvents(false);
-    }
-
-    private Set getFrequent(BufferedReader br) {
-	HashMap map = new HashMap();
-
-	try {
-	    for(String s = br.readLine(); s!=null; s=br.readLine()) {
-		StringTokenizer st = new StringTokenizer(s);
-		while(st.hasMoreTokens()) {
-		    String tok = (String)split(st.nextToken()).a;
-		    Counter c = (Counter)map.get(tok);
-		    if(c!=null)
-			c.increment();
-		    else
-			map.put(tok, new Counter());
-		}
-	    }
-	} catch (IOException e) { e.printStackTrace(); }
-
-	HashSet set = new HashSet();
-	for(Iterator i=map.entrySet().iterator(); i.hasNext();) {
-	    Map.Entry entry = (Map.Entry)i.next();
-	    if(((Counter)entry.getValue()).passesCutoff(5))
-		set.add(entry.getKey());
-	}
-
-	return set;
+    HashSet set = new HashSet();
+    for(Iterator i=map.entrySet().iterator(); i.hasNext();) {
+      Map.Entry entry = (Map.Entry)i.next();
+      if(((Counter)entry.getValue()).passesCutoff(5))
+        set.add(entry.getKey());
     }
     
-    /** 
-     * Builds up the list of features using the Reader as input.  For now, this
-     * should only be used to create training data.
-     */
-    public Event[] getEvents(boolean evalMode) {
-	ArrayList elist = new ArrayList();
-	int numMatches;
-	//Set frequent;
-	/*
-	if(!evalMode) {
-	  //System.out.println("Reading in all the data");
-	    try {
-		StringBuffer sb = new StringBuffer();
-		for(String s = br.readLine(); s!=null; s=br.readLine())
-		    sb.append(s+"\n");
-		//System.out.println("Getting most frequent words");
-		frequent =
-		    getFrequent(new BufferedReader(
-				     new StringReader(sb.toString())));
-		br = new BufferedReader(new StringReader(sb.toString()));
-		sb=null;
-	    } catch (IOException e) { e.printStackTrace(); }
-	}
-	*/
-	//System.out.println("Collecting events");
-	try {
-	    String s = br.readLine();
-	    
-	    while (s != null) {
-		Pair p = convertAnnotatedString(s);
-		ArrayList tokens = (ArrayList)p.a;
-		ArrayList outcomes = (ArrayList)p.b;
-		ArrayList tags = new ArrayList();
-		
-		for (int i=0; i<tokens.size(); i++) {
-		    String[] context = cg.getContext(i,tokens.toArray(),(String[]) tags.toArray(new String[tags.size()]),null);
-		    Event e = new Event((String)outcomes.get(i), context);
-		    tags.add(outcomes.get(i));
-		    elist.add(e);
-		}
-		s = br.readLine();
-	    }
-	} catch (Exception e) { e.printStackTrace(); }
-
-	Event[] events = new Event[elist.size()];
-        for(int i=0; i<events.length; i++)
-            events[i] = (Event)elist.get(i);
- 
-        return events;
-    }
-
-    public static void main(String[] args) {
-	String data = "the_DT stories_NNS about_IN well-heeled_JJ communities_NNS and_CC developers_NNS";
-	EventCollector ec = new POSEventCollector(new StringReader(data),
-						  new DefaultPOSContextGenerator());
-	Event[] events = ec.getEvents();
-	for(int i=0; i<events.length; i++)
-	    System.out.println(events[i].getOutcome());
-    }
+    return set;
+  }
+  
+  /** 
+   * Builds up the list of features using the Reader as input.  For now, this
+   * should only be used to create training data.
+   */
+  public Event[] getEvents(boolean evalMode) {
+    ArrayList elist = new ArrayList();
+    int numMatches;
+    //Set frequent;
+    /*
+     if(!evalMode) {
+     //System.out.println("Reading in all the data");
+      try {
+      StringBuffer sb = new StringBuffer();
+      for(String s = br.readLine(); s!=null; s=br.readLine())
+      sb.append(s+"\n");
+      //System.out.println("Getting most frequent words");
+       frequent =
+       getFrequent(new BufferedReader(
+       new StringReader(sb.toString())));
+       br = new BufferedReader(new StringReader(sb.toString()));
+       sb=null;
+       } catch (IOException e) { e.printStackTrace(); }
+       }
+       */
+    //System.out.println("Collecting events");
+    try {
+      String s = br.readLine();
+      
+      while (s != null) {
+        Pair p = convertAnnotatedString(s);
+        ArrayList tokens = (ArrayList)p.a;
+        ArrayList outcomes = (ArrayList)p.b;
+        ArrayList tags = new ArrayList();
+        
+        for (int i=0; i<tokens.size(); i++) {
+          String[] context = cg.getContext(i,tokens.toArray(),(String[]) tags.toArray(new String[tags.size()]),null);
+          Event e = new Event((String)outcomes.get(i), context);
+          tags.add(outcomes.get(i));
+          elist.add(e);
+        }
+        s = br.readLine();
+      }
+    } catch (Exception e) { e.printStackTrace(); }
     
+    Event[] events = new Event[elist.size()];
+    for(int i=0; i<events.length; i++)
+      events[i] = (Event)elist.get(i);
+    
+    return events;
+  }
+  
+  public static void main(String[] args) {
+    String data = "the_DT stories_NNS about_IN well-heeled_JJ communities_NNS and_CC developers_NNS";
+    EventCollector ec = new POSEventCollector(new StringReader(data),
+        new DefaultPOSContextGenerator());
+    Event[] events = ec.getEvents();
+    for(int i=0; i<events.length; i++)
+      System.out.println(events[i].getOutcome());
+  }
+  
 }
