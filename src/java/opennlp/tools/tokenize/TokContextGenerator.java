526dbd7e21f873cddc695cf6570d4aee196cd219
tsmorton <tsmorton>
2006-01-16 17:49:00 +0000

    Made getContext method with explicit arguments.

--- src/java/opennlp/tools/tokenize/TokContextGenerator.java
+++ src/java/opennlp/tools/tokenize/TokContextGenerator.java
@@ -29,85 +29,98 @@ import java.util.ArrayList;
  * Generate events for maxent decisions for tokenization.
  *
  * @author      Jason Baldridge
- * @version     $Revision: 1.2 $, $Date: 2004/01/26 14:16:37 $
+ * @version     $Revision: 1.3 $, $Date: 2006/01/16 17:49:00 $
  */
 
 public class TokContextGenerator implements ContextGenerator {
 
-    public static final String SPLIT ="T";
-    public static final String NO_SPLIT ="F";
+  public static final String SPLIT ="T";
+  public static final String NO_SPLIT ="F";
+  
+  /**
+   * Builds up the list of features based on the information in the Object,
+   * which is a pair containing a String and and Integer which
+   * indicates the index of the position we are investigating.
+   */
+  public String[] getContext(Object o) {
+    String sb = (String)((ObjectIntPair)o).a;
+    int id = ((ObjectIntPair)o).b;
+    return getContext(sb,id);
+  }
 
-    /**
-     * Builds up the list of features based on the information in the Object,
-     * which is a pair containing a String and and Integer which
-     * indicates the index of the position we are investigating.
-     */
-    public String[] getContext(Object o) {	
-	String sb = (String)((ObjectIntPair)o).a;
-	int id = ((ObjectIntPair)o).b;
-		
-	List preds = new ArrayList();
-	preds.add("p="+sb.substring(0,id));
-	preds.add("s="+sb.substring(id));
-	if (id>0) {
-	    addCharPreds("p1", sb.charAt(id-1), preds);
-	    if (id>1) {
-		addCharPreds("p2", sb.charAt(id-2), preds);
-		preds.add("p21="+sb.charAt(id-2)+sb.charAt(id-1));
-	    }
-	    else {
-	        preds.add("p2=bok");
-	    }
-	    preds.add("p1f1="+sb.charAt(id-1)+sb.charAt(id));
-	}
-	else {
-	    preds.add("p1=bok");
-	}
-	addCharPreds("f1",sb.charAt(id), preds);
-	if (id+1 < sb.length()) {
-	    addCharPreds("f2", sb.charAt(id+1), preds);
-	    preds.add("f12="+sb.charAt(id)+sb.charAt(id+1));
-	}
-	else {
-	    preds.add("f2=bok");
-	}
-	if (sb.charAt(0) == '&' && sb.charAt(sb.length()-1) == ';') {
-	  preds.add("cc");//character code
-	}
-
-	String[] context = new String[preds.size()];
-	preds.toArray(context);
-	return context;
+  /**
+   * Returns an array of features for the specified sentence string at the specified index.
+   * @param sentence The string for a sentence.
+   * @param index The index to consider spliting as a token.
+   * @return an array of features for the specified sentence string at the specified index.
+   */
+  public String[] getContext(String sentence, int index) {	
+    List preds = new ArrayList();
+    preds.add("p="+sentence.substring(0,index));
+    preds.add("s="+sentence.substring(index));
+    if (index>0) {
+      addCharPreds("p1", sentence.charAt(index-1), preds);
+      if (index>1) {
+        addCharPreds("p2", sentence.charAt(index-2), preds);
+        preds.add("p21="+sentence.charAt(index-2)+sentence.charAt(index-1));
+      }
+      else {
+        preds.add("p2=bok");
+      }
+      preds.add("p1f1="+sentence.charAt(index-1)+sentence.charAt(index));
+    }
+    else {
+      preds.add("p1=bok");
+    }
+    addCharPreds("f1",sentence.charAt(index), preds);
+    if (index+1 < sentence.length()) {
+      addCharPreds("f2", sentence.charAt(index+1), preds);
+      preds.add("f12="+sentence.charAt(index)+sentence.charAt(index+1));
+    }
+    else {
+      preds.add("f2=bok");
+    }
+    if (sentence.charAt(0) == '&' && sentence.charAt(sentence.length()-1) == ';') {
+      preds.add("cc");//character code
     }
     
+    String[] context = new String[preds.size()];
+    preds.toArray(context);
+    return context;
+  }
+    
 
-    /**
-     * Helper function for getContext.
-     */
-    private void addCharPreds(String key, char c, List preds) {
-	preds.add(key + "=" + c);
-	if (Character.isLetter(c)) {
-	    preds.add(key+"_alpha");
-	    if (Character.isUpperCase(c)) {
-		preds.add(key+"_caps");
-	    }
-	} else if (Character.isDigit(c)) {
-	    preds.add(key+"_num");
-	} else if (Character.isWhitespace(c)) {
-	    preds.add(key+"_ws");
-	} else {
-	    if (c=='.' || c=='?' || c=='!') {
-		preds.add(key+"_eos");
-	    } else if (c=='`' || c=='"' || c=='\'') {
-		preds.add(key+"_quote");
-	    } else if (c=='[' || c=='{' || c=='(') {
-		preds.add(key+"_lp");
-	    } else if (c==']' || c=='}' || c==')') {
-		preds.add(key+"_rp");
-	    }
-	}
+  /**
+   * Helper function for getContext.
+   */
+  private void addCharPreds(String key, char c, List preds) {
+    preds.add(key + "=" + c);
+    if (Character.isLetter(c)) {
+      preds.add(key+"_alpha");
+      if (Character.isUpperCase(c)) {
+        preds.add(key+"_caps");
+      }
+    } 
+    else if (Character.isDigit(c)) {
+      preds.add(key+"_num");
+    } 
+    else if (Character.isWhitespace(c)) {
+      preds.add(key+"_ws");
+    } 
+    else {
+      if (c=='.' || c=='?' || c=='!') {
+        preds.add(key+"_eos");
+      } 
+      else if (c=='`' || c=='"' || c=='\'') {
+        preds.add(key+"_quote");
+      } 
+      else if (c=='[' || c=='{' || c=='(') {
+        preds.add(key+"_lp");
+      } 
+      else if (c==']' || c=='}' || c==')') {
+        preds.add(key+"_rp");
+      }
     }
-
- 
+  }
 }
 
