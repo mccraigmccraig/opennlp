fd131914f16ca163a5d086a1387dff2eed4e490c
tsmorton <tsmorton>
2006-01-16 17:42:28 +0000

    updated to reflect parser changes and call new parser types.

--- src/java/opennlp/tools/lang/english/TreebankParser.java
+++ src/java/opennlp/tools/lang/english/TreebankParser.java
@@ -29,8 +29,9 @@ import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 import opennlp.maxent.io.SuffixSensitiveGISModelReader;
+import opennlp.tools.parser.AbstractBottomUpParser;
 import opennlp.tools.parser.Parse;
-import opennlp.tools.parser.ParserME;
+import opennlp.tools.parser.Parser;
 import opennlp.tools.util.Span;
 
 /**
@@ -40,9 +41,11 @@ public class TreebankParser {
 
   private static Pattern untokenizedParenPattern1 = Pattern.compile("([^ ])([({)}])");
   private static Pattern untokenizedParenPattern2 = Pattern.compile("([({)}])([^ ])");
-  public static ParserME getParser(String dataDir, boolean useTagDictionary, boolean useCaseSensitiveTagDictionary, int beamSize, double advancePercentage) throws IOException {
+  
+  
+  public static Parser getParser(String dataDir, boolean useTagDictionary, boolean useCaseSensitiveTagDictionary, int beamSize, double advancePercentage) throws IOException {
     if (useTagDictionary) {
-      return new ParserME(
+      return new opennlp.tools.parser.chunking.Parser(
         new SuffixSensitiveGISModelReader(new File(dataDir + "/build.bin.gz")).getModel(),
         new SuffixSensitiveGISModelReader(new File(dataDir + "/check.bin.gz")).getModel(),
         new ParserTagger(dataDir + "/tag.bin.gz", dataDir + "/tagdict", useCaseSensitiveTagDictionary ),//, new Dictionary(dataDir+"/dict.bin.gz")),
@@ -50,7 +53,7 @@ public class TreebankParser {
         new HeadRules(dataDir + "/head_rules"),beamSize,advancePercentage);
     }
     else {
-      return new ParserME(
+      return new opennlp.tools.parser.chunking.Parser(
         new SuffixSensitiveGISModelReader(new File(dataDir + "/build.bin.gz")).getModel(),
         new SuffixSensitiveGISModelReader(new File(dataDir + "/check.bin.gz")).getModel(),
         new ParserTagger(dataDir + "/tag.bin.gz",null), //new Dictionary(dataDir+"/dict.bin.gz")),
@@ -59,8 +62,21 @@ public class TreebankParser {
     }
   }
   
-  public static ParserME getParser(String dataDir) throws IOException {
-    return getParser(dataDir,true,true,ParserME.defaultBeamSize,ParserME.defaultAdvancePercentage);
+  public static Parser getParser(String dataDir) throws IOException {
+    return getParser(dataDir,true,true,AbstractBottomUpParser.defaultBeamSize,AbstractBottomUpParser.defaultAdvancePercentage);
+  }
+  
+  public static Parser getInsertionParser(String dataDir) throws IOException {
+    return getInsertionParser(dataDir,true,true,AbstractBottomUpParser.defaultBeamSize,AbstractBottomUpParser.defaultAdvancePercentage);
+  }
+  
+  public static Parser getInsertionParser(String dataDir, boolean useTagDictionary, boolean useCaseSensitiveTagDictionary, int beamSize, double advancePercentage) throws IOException {
+    return new opennlp.tools.parser.treeinsert.Parser(
+        new SuffixSensitiveGISModelReader(new File(dataDir + "/build.bin.gz")).getModel(),
+        new SuffixSensitiveGISModelReader(new File(dataDir + "/attach.bin.gz")).getModel(),
+        new ParserTagger(dataDir + "/tag.bin.gz", dataDir + "/tagdict", useCaseSensitiveTagDictionary ),//, new Dictionary(dataDir+"/dict.bin.gz")),
+        new ParserChunker(dataDir + "/chunk.bin.gz"),
+        new HeadRules(dataDir + "/head_rules"),beamSize,advancePercentage);
   }
   
   private static String convertToken(String token) {
@@ -79,7 +95,7 @@ public class TreebankParser {
     return token;
   }
   
-  public static Parse[] parseLine(String line, ParserME parser, int numParses) {
+  public static Parse[] parseLine(String line, Parser parser, int numParses) {
     line = untokenizedParenPattern1.matcher(line).replaceAll("$1 $2");
     line = untokenizedParenPattern2.matcher(line).replaceAll("$1 $2");
     StringTokenizer str = new StringTokenizer(line);
@@ -91,11 +107,12 @@ public class TreebankParser {
       sb.append(tok).append(" ");
     }
     String text = sb.substring(0, sb.length() - 1).toString();
-    Parse p = new Parse(text, new Span(0, text.length()), "INC", 1, null);
+    Parse p = new Parse(text, new Span(0, text.length()), AbstractBottomUpParser.INC_NODE, 1, 0);
     int start = 0;
-    for (Iterator ti = tokens.iterator(); ti.hasNext();) {
+    int i=0;
+    for (Iterator ti = tokens.iterator(); ti.hasNext();i++) {
       String tok = (String) ti.next();
-      p.insert(new Parse(text, new Span(start, start + tok.length()), ParserME.TOK_NODE, 0));
+      p.insert(new Parse(text, new Span(start, start + tok.length()), AbstractBottomUpParser.TOK_NODE, 0,i));
       start += tok.length() + 1;
     }
     Parse[] parses;
@@ -109,8 +126,9 @@ public class TreebankParser {
   }
 
   private static void usage() {
-    System.err.println("Usage: TreebankParser -[id] -bs -ap dataDirectory < tokenized_sentences");
+    System.err.println("Usage: TreebankParser [-d -i -bs n -ap f -type t] dataDirectory < tokenized_sentences");
     System.err.println("dataDirectory: Directory containing parser models.");
+    System.err.println("-type [chunking|insertion]: Type of parser to use.");
     System.err.println("-d: Use tag dictionary.");
     System.err.println("-i: Case insensitive tag dictionary.");
     System.err.println("-bs 20: Use a beam size of 20.");
@@ -127,15 +145,20 @@ public class TreebankParser {
     boolean useTagDictionary = false;
     boolean caseSensitiveTagDictionary = true;
     boolean showTopK = false;
+    String parserType = "chunking";
     int numParses = 1;
     int ai = 0;
-    int beamSize = ParserME.defaultBeamSize;
-    double advancePercentage = ParserME.defaultAdvancePercentage;
+    int beamSize = AbstractBottomUpParser.defaultBeamSize;
+    double advancePercentage = AbstractBottomUpParser.defaultAdvancePercentage;
     while (args[ai].startsWith("-")) {
       if (args[ai].equals("-d")) {
         useTagDictionary = true;
       }
       else if (args[ai].equals("-i")) {
+        caseSensitiveTagDictionary = false;
+      }
+      else if (args[ai].equals("-di") || args[ai].equals("-id")) {
+        useTagDictionary = true;
         caseSensitiveTagDictionary = false;
       }
       else if (args[ai].equals("-bs")) {
@@ -184,6 +207,18 @@ public class TreebankParser {
       	  usage();
       	}
       }
+      else if (args[ai].equals("-type")) {
+        if (args.length > ai+1) {
+          parserType = args[ai+1];
+          ai++;
+          if (!parserType.equals("chunking") && !parserType.equals("insertion")) {
+            usage();
+          }
+        }
+        else {
+          usage();
+        }
+      }
       else if (args[ai].equals("--")) {
       	ai++;
         break;
@@ -194,15 +229,20 @@ public class TreebankParser {
       }
       ai++;
     }
-    ParserME parser;
-    if (!caseSensitiveTagDictionary) {
-      parser = TreebankParser.getParser(args[ai++], true, false,beamSize,advancePercentage);
+    Parser parser = null;
+    if (parserType.equals("chunking")) {
+      if (!caseSensitiveTagDictionary) {
+        parser = TreebankParser.getParser(args[ai++], true, false,beamSize,advancePercentage);
+      }
+      else if (useTagDictionary) {
+        parser = TreebankParser.getParser(args[ai++], true, true,beamSize,advancePercentage);
+      }
+      else {
+        parser = TreebankParser.getParser(args[ai++], false, false,beamSize,advancePercentage);
+      }
     }
-    else if (useTagDictionary) {
-      parser = TreebankParser.getParser(args[ai++], true, true,beamSize,advancePercentage);
-    }
-    else {
-      parser = TreebankParser.getParser(args[ai++], false, false,beamSize,advancePercentage);
+    else if (parserType.equals("insertion")) {
+      parser = TreebankParser.getInsertionParser(args[ai++], false, false,beamSize,advancePercentage);
     }
     BufferedReader in;
     if (ai == args.length) {
