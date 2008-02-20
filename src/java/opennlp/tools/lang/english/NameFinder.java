1fe92cb0d03349c679b26b375e88bedc70365f26
tsmorton <tsmorton>
2008-02-20 18:22:56 +0000

    removed some un-used imports.

--- src/java/opennlp/tools/lang/english/NameFinder.java
+++ src/java/opennlp/tools/lang/english/NameFinder.java
@@ -21,8 +21,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 import opennlp.maxent.MaxentModel;
 import opennlp.maxent.io.BinaryGISModelReader;
