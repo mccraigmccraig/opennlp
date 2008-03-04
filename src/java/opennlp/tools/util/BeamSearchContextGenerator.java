83baed5e81c9a34359187a9c62bb57926addd408
joernkottmann <joernkottmann>
2008-03-04 10:26:39 +0000

    javadoc typo

--- src/java/opennlp/tools/util/BeamSearchContextGenerator.java
+++ src/java/opennlp/tools/util/BeamSearchContextGenerator.java
@@ -26,7 +26,7 @@ public interface BeamSearchContextGenerator extends ContextGenerator {
     
   /** Returns the context for the specified position in the specified sequence (list).  
      * @param index The index of the sequence.
-     * @param sequence  The sequence of items over which the beam seeach is performed.
+     * @param sequence  The sequence of items over which the beam search is performed.
      * @param priorDecisions The sequence of decisions made prior to the context for which this decision is being made.
      * @param additionalContext Any addition context specific to a class implementing this interface.
      * @return the context for the specified position in the specified sequence.
