543361709453ef222fc8538612edad3b4f64dd26
joernkottmann <joernkottmann>
2008-08-13 00:11:16 +0000

    Added LGPL header

--- src/java/opennlp/tools/postag/POSTagger.java
+++ src/java/opennlp/tools/postag/POSTagger.java
@@ -1,3 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+//Copyright (C) 2008 OpenNlp
+// 
+//This library is free software; you can redistribute it and/or
+//modify it under the terms of the GNU Lesser General Public
+//License as published by the Free Software Foundation; either
+//version 2.1 of the License, or (at your option) any later version.
+// 
+//This library is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Lesser General Public License for more details.
+// 
+//You should have received a copy of the GNU Lesser General Public
+//License along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//////////////////////////////////////////////////////////////////////////////
+
 package opennlp.tools.postag;
 
 import java.util.List;
@@ -6,7 +24,7 @@ import java.util.List;
  * The interface for part of speech taggers.
  *
  * @author      Jason Baldridge
- * @version $Revision: 1.3 $, $Date: 2008/04/19 21:12:25 $ */
+ * @version $Revision: 1.4 $, $Date: 2008/08/13 00:11:16 $ */
 
 public interface POSTagger {
 
--- src/java/opennlp/tools/postag/TagDictionary.java
+++ src/java/opennlp/tools/postag/TagDictionary.java
@@ -1,15 +1,37 @@
+///////////////////////////////////////////////////////////////////////////////
+//Copyright (C) 2008 OpenNlp
+// 
+//This library is free software; you can redistribute it and/or
+//modify it under the terms of the GNU Lesser General Public
+//License as published by the Free Software Foundation; either
+//version 2.1 of the License, or (at your option) any later version.
+// 
+//This library is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Lesser General Public License for more details.
+// 
+//You should have received a copy of the GNU Lesser General Public
+//License along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//////////////////////////////////////////////////////////////////////////////
+
 package opennlp.tools.postag;
 
-/** Interface to determine which tags are valid for a particular word based on a tag dictionary.
+/** 
+ * Interface to determine which tags are valid for a particular word
+ * based on a tag dictionary.
+ * 
  * @author Tom Morton
  */
 public interface TagDictionary {
   
   /**
-   * Returns a list of valid tags for the specified word. 
+   * Returns a list of valid tags for the specified word.
+   * 
    * @param word The word.
-   * @return A list of valid tags for the specified word or null if no information is available for that word.
+   * @return A list of valid tags for the specified word or null if no information
+   * is available for that word.
    */
   public String[] getTags(String word);
-  
 }
