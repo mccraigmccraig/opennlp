e12d435da5f72ee6f3ed56c0b7f61b7dcc10420b
joernkottmann <joernkottmann>
2008-08-14 15:05:26 +0000

    Implemented serialize and create methods

--- src/java/opennlp/tools/chunker/ChunkerModel.java
+++ src/java/opennlp/tools/chunker/ChunkerModel.java
@@ -18,8 +18,19 @@
 
 package opennlp.tools.chunker;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
 import opennlp.maxent.GISModel;
 import opennlp.maxent.MaxentModel;
+import opennlp.maxent.io.BinaryGISModelReader;
+import opennlp.tools.util.InvalidFormatException;
+import opennlp.tools.util.ModelUtil;
 
 /**
  * The {@link ChunkerModel} is the model used
@@ -29,6 +40,8 @@ import opennlp.maxent.MaxentModel;
  */
 public class ChunkerModel {
 
+  private static final String CHUNKER_MODEL_ENTRY_NAME = "chunker.bin";
+  
   private GISModel chunkerModel;
   
   public ChunkerModel(GISModel chunkerModel) {
@@ -37,5 +50,49 @@ public class ChunkerModel {
   
   public MaxentModel getMaxentChunkerModel() {
     return chunkerModel;
+  }
+  
+  /**
+   * .
+   * 
+   * After the serialization is finished the provided 
+   * {@link OutputStream} is closed.
+   * 
+   * @param out
+   * @throws IOException
+   */
+  public void serialize(OutputStream out) throws IOException {
+    ZipOutputStream zip = new ZipOutputStream(out);
+    
+    zip.putNextEntry(new ZipEntry(CHUNKER_MODEL_ENTRY_NAME));
+    ModelUtil.writeModel(chunkerModel, zip);
+    zip.closeEntry();
+    
+    zip.close();
+  }
+  
+  /**
+   * .
+   * 
+   * The {@link InputStream} in remains open after the model is read.
+   * 
+   * @param in
+   * @return
+   * @throws IOException
+   * @throws InvalidFormatException
+   */
+  public static ChunkerModel create(InputStream in) throws IOException, InvalidFormatException {
+    ZipInputStream zip = new ZipInputStream(in);
+    
+    ZipEntry chunkerModelEntry = zip.getNextEntry();
+    
+    if (chunkerModelEntry == null || 
+        !CHUNKER_MODEL_ENTRY_NAME.equals(chunkerModelEntry.getName()))
+      throw new InvalidFormatException("Could not find maxent chunker model!");
+    
+    GISModel chunkerModel = new BinaryGISModelReader(
+        new DataInputStream(zip)).getModel();
+    
+    return new ChunkerModel(chunkerModel);
   }
 }
